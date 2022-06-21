f4b66af7460abc12fd6c5b9cf5e1c589733d1113
Daniel Lam <47636806+dpryde258@users.noreply.github.com>
2022-06-21 21:26:35 +0700

    feat(sdk-swap): add sdk initialization

--- packages/sdk-swap/src/lib/sdk-swap.ts
+++ packages/sdk-swap/src/lib/sdk-swap.ts
@@ -1,3 +1,28 @@
-export function sdkSwap(): string {
-  return "sdk-swap";
+import { ApiPromise } from "@polkadot/api";
+import { AnyNumber, Result, ContractAddress } from "./types";
+
+/**
+ * Swap SDK is created to facilitate the integration with BHO Swap contracts.
+ */
+export class SwapSdk {
+  private constructor(
+    private _api: ApiPromise,
+    private _routerAddr: ContractAddress,
+    private _factoryAddr: ContractAddress
+  ) {}
+
+  /**
+   * Initialize the SDK instance
+   * @param api - Polkadot `ApiPromise` instance. Consumers should initialize this api instance beforehand.
+   * @param routerAddr - BHO Swap Router contract address.
+   * @param factoryAddr - BHO Swap Factory contract address.
+   * @returns Returns initialized `{@link SwapSdk | Swap SDK instance}
+   */
+  static initialize(
+    api: ApiPromise,
+    routerAddr: ContractAddress,
+    factoryAddr: ContractAddress
+  ): SwapSdk {
+    return new SwapSdk(api, routerAddr, factoryAddr);
+  }
 }
---
+++ packages/sdk-swap/src/lib/types.ts
@@ -0,0 +1,5 @@
+import { AnyNumber } from "@polkadot/types/types";
+
+export type { AnyNumber } from "@polkadot/types/types";
+export type { Result } from "defekt";
+export type ContractAddress = string | Uint8Array | `0x${string}`;
